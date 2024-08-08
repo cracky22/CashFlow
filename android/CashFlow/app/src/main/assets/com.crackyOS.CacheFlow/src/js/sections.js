074f585a37503117aa8d99fca48c6b35455c4890
Martin Blieninger <martinblieninger2208@gmail.com>
2024-08-08 16:41:48 +0200

    "Updated Android app configuration, layout, and assets; added JavaScript-enabled WebView and navigation logic"

---
+++ android/CashFlow/app/src/main/assets/com.crackyOS.CacheFlow/src/js/sections.js
@@ -0,0 +1,13 @@
+function show_section(section) {
+    const sections = ['mainSection', 'historySection', 'manageSection', 'pinSection'];
+    
+    sections.forEach(sec => {
+        const element = document.getElementById(sec);
+        if (sec === section) {
+            element.classList.add("active");
+        } else {
+            element.classList.remove("active");
+        }
+    });
+    document.getElementById("masterTitle").innerHTML = `CashFlow <i><small>[${section}]</small></i>`;
+}
