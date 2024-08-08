cde19c45e169deaee1f88efd0d484a08f7db318c
Martin Blieninger <martinblieninger2208@gmail.com>
2024-08-08 17:40:02 +0200

    "Updated HTML, CSS, and JavaScript files in CashFlow app to add transaction forms and functionality"

---
+++ android/CashFlow/app/src/main/assets/com.crackyOS.CacheFlow/src/js/decrease.js
@@ -0,0 +1,19 @@
+document.getElementById('addForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const reason = document.getElementById('reason').value.trim();
+    const value = parseFloat(document.getElementById('value').value);
+
+    if (reason && !isNaN(value)) {
+        const date = new Date().toISOString().split('T')[0];
+        const formattedValue = `-${value}`;
+        const newEntry = { date, reason, value: formattedValue };
+        let balanceHistory = JSON.parse(localStorage.getItem('balanceHistory')) || [];
+        balanceHistory.push(newEntry);
+        localStorage.setItem('balanceHistory', JSON.stringify(balanceHistory));
+        document.getElementById('addForm').reset();
+        alert('Transaktion hinzugefügt!');
+    } else {
+        alert('Bitte alle Felder korrekt ausfüllen.');
+    }
+});
--- android/CashFlow/app/src/main/assets/com.crackyOS.CacheFlow/src/js/increase.js
+++ android/CashFlow/app/src/main/assets/com.crackyOS.CacheFlow/src/js/increase.js
@@ -1,24 +1,17 @@
-// Formular-Submit-Event-Listener
 document.getElementById('addForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Verhindert das Standard-Formular-Absenden
+    event.preventDefault();
 
     const reason = document.getElementById('reason').value.trim();
     const value = parseFloat(document.getElementById('value').value);
 
     if (reason && !isNaN(value)) {
-        const date = new Date().toISOString().split('T')[0]; // Aktuelles Datum im Format YYYY-MM-DD
-        const formattedValue = `+${value}`; // Wert formatieren
-
+        const date = new Date().toISOString().split('T')[0];
+        const formattedValue = `+${value}`;
         const newEntry = { date, reason, value: formattedValue };
-
-        // Alte Daten aus LocalStorage holen
         let balanceHistory = JSON.parse(localStorage.getItem('balanceHistory')) || [];
         balanceHistory.push(newEntry);
         localStorage.setItem('balanceHistory', JSON.stringify(balanceHistory));
-
-        // Formular zurücksetzen
         document.getElementById('addForm').reset();
-
         alert('Transaktion hinzugefügt!');
     } else {
         alert('Bitte alle Felder korrekt ausfüllen.');
